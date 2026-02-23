9f9b132624c793e8db3737bd349acb713cf25ce9
Thomas Wong <wonghk.thomas@gmail.com>
2026-02-23 11:24:38 +0800

    Add helper functions for checking last import date and  prev_ver

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import os
 import threading
 import requests
+import json
+import pandas as pd
+from pathlib import Path
+import re
 from flask import Flask, jsonify, request
 from main import main_combined as fetch_link_main
 from process_data import main_combined as process_data
@@ -108,6 +112,60 @@ def trigger_process_data():
         "service": SERVICE_NAME
     }
     return jsonify(response), 202
+
+
+@app.route('/get_last_import_date', methods=['GET'])
+def get_last_import_date():
+    with open(os.path.join(BASE_PATH, "update_masterlist.json"),
+              "r", encoding="utf-8") as f:
+        config = json.load(f)
+    excel_config = config["excel_config"]
+    excel_path = excel_config["excel_file"]
+    sheet_configs = excel_config["sheets"]
+    excel_data = pd.read_excel(excel_path, sheet_name=None, engine="openpyxl")
+    res = {}
+    for sheet_config in sheet_configs:
+        sheet_name = sheet_config["sheet_name"]
+        if not sheet_name.startswith("LLM_"):
+            master_df = excel_data[sheet_name]
+            import_date = master_df["import_date"]
+            res[sheet_name] = import_date.max().strftime("%Y-%m-%d")
+
+    response = {
+        "status": "success",
+        "service": SERVICE_NAME,
+        "result": res
+    }
+    return jsonify(response), 200
+
+
+def find_latest_date_dir(parent_dir: str | Path) -> Path | None:
+    parent = Path(parent_dir)
+    date_regex = re.compile(r'^\d{8}$')
+    candidates = [
+        p for p in parent.iterdir()
+        if p.is_dir() and date_regex.match(p.name)
+    ]
+    return max(candidates, key=lambda p: p.name).name
+
+
+@app.route('/get_prev_ver', methods=['GET'])
+def get_prev_ver():
+    eng_path = os.path.join(BASE_PATH, "..", "output", "English")
+    chi_path = os.path.join(BASE_PATH, "..", "output", "Chinese")
+
+    eng_prev_ver = find_latest_date_dir(eng_path)
+    chi_prev_ver = find_latest_date_dir(chi_path)
+
+    response = {
+        "status": "success",
+        "service": SERVICE_NAME,
+        "result": {
+            "eng": str(eng_prev_ver),
+            "chi": str(chi_prev_ver)
+        }
+    }
+    return jsonify(response), 200
 
 
 if __name__ == "__main__":
